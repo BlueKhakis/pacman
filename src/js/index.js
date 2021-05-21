49b8b731922c08a6832b2fa9fe2f5e00a0f0b270
Matthew <matthew.lynewatt@gmail.com>
2021-05-21 11:25:35 +0200

    third commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,24 +2,55 @@
 
 // const htmlmin = require("gulp-htmlmin");
 
+const TILE_SIZE = 85;
 
 const pacmanMouth  = document.querySelector(".pacboy-active-light");
-const body  = document.querySelector('body');
 
-let toggle = pacmanMouth.style.backgroundPositionX = "right"
 
-const toggle = () => {
-    
-}
+let mouthOpen = true;
 
 
  document.addEventListener('keydown', (event) => {
     if(event.code === 'ArrowRight') {
-        if(pacmanMouth.style.backgroundPositionX === "left") {
-        pacmanMouth.style.backgroundPositionX = "right";  
+        if(pacmanMouth.style.backgroundPositionX === 0 + "%") {
+        pacmanMouth.style.backgroundPositionX = 100 + "%"; 
+        mouthOpen = false; 
         } else {
-        pacmanMouth.style.backgroundPositionX = "left"
+        pacmanMouth.style.backgroundPositionX = 0 + "%"
+        mouthOpen = true; 
   }
     }
 })
+
+
+
+class Pacman {
+    constructor(xpos, mouthOpen){
+        this.xpos = xpos;
+        this.mouthOpen = mouthOpen;
+
+    }
+render() {
+    document.addEventListener('keydown', (event) => {
+    if(event.code === 'ArrowRight') {
+        this.moveRight()
+    }
+})
+}
+mount() {
+
+}
+update(){
+    this.moveRight()
+
+}
+moveRight(){
+    this.xpos = 0
+        this.xpos += TILE_SIZE
+        pacmanMouth.style.left = this.xpos + "px"
+
+}
+}
+
+
 
