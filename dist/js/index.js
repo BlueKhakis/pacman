99a4540eb6c89cb001936015086d36fcbc69133d
Matthew <matthew.lynewatt@gmail.com>
2021-05-21 10:36:00 +0200

    commit

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('it works!');
+// const htmlmin = require("gulp-htmlmin");
+
+
+const pacmanMouth  = document.querySelector(".pacboy-active-light");
+const body  = document.querySelector('body');
+
+let toggle = pacmanMouth.style.backgroundPositionX = "right"
+
+const toggle = () => {
+    
+}
+
+
+ document.addEventListener('keydown', (event) => {
+    if(event.code === 'ArrowRight') {
+        if(pacmanMouth.style.backgroundPositionX === "left") {
+        pacmanMouth.style.backgroundPositionX = "right";  
+        } else {
+        pacmanMouth.style.backgroundPositionX = "left"
+  }
+    }
+})
+
